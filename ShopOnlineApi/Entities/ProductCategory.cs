21a069459fa24625d757b483d7404ccee949e9ef
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-10-07 11:52:47 +0530

    Add project files.

---
+++ ShopOnlineApi/Entities/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnlineApi.Entity
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+
+    }
+}
---
+++ ShopOnlineApi/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnlineApi.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
